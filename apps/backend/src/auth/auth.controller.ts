2048567d3ccb3b41d42a5cac759e230d3a359c8f
Jon Higgins <jon.higgins@fictioneers.co.uk>
2022-02-21 17:09:33 +0000

    Update auth route

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Param, Get } from '@nestjs/common';
+import { Controller, Param, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) { }
 
-  @Get('get-token-for-user/:id')
+  @Post('token/:id')
   async getTokenForUser(@Param() params) {
     const userId = params.id
     return this.authService.getTokenForUser(userId)
