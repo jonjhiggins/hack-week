1b2953538ef35279f3350443b737c94adc72bb39
Jon Higgins <jon.higgins@fictioneers.co.uk>
2022-02-22 15:39:11 +0000

    Add swagger docs for openapi

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,13 +3,15 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
+import { writeFileSync } from 'fs';
 
 // should do this as a global service
 // e.g. https://docs.nestjs.com/techniques/configuration
-const envVars = ['FICTIONEERS_API_BASE_URL', 'FICTIONEERS_API_KEY', 'SESSION_SECRET']
+const envVars = ['FICTIONEERS_API_BASE_URL', 'FICTIONEERS_API_KEY', 'SESSION_SECRET', 'SHOW_DOCS']
 
 envVars.forEach(e => {
   if (!process.env[e]) {
@@ -17,12 +19,29 @@ envVars.forEach(e => {
   }
 })
 
+function docs(app: INestApplication) {
+  // Docs / Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Hack week API')
+    .setDescription('Team newbie')
+    .setVersion('1.0')
+    .addTag('hack-week')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  writeFileSync('open-api.json', JSON.stringify(document))
+  SwaggerModule.setup('api', app, document);
+}
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  if (process.env.SHOW_DOCS && process.env.SHOW_DOCS === 'true') {
+    docs(app)
+  }
 
   await app.listen(port);
   Logger.log(
