9514fb0ee95d55f010f6bac3845e2cdbbce7b8c5
Jon Higgins <jon.higgins@fictioneers.co.uk>
2022-02-22 11:44:07 +0000

    Store session in cookie

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,11 +8,34 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
+import * as session from 'express-session';
+
+// should do this as a global service
+// e.g. https://docs.nestjs.com/techniques/configuration
+const envVars = ['FICTIONEERS_API_BASE_URL', 'FICTIONEERS_API_KEY', 'SESSION_SECRET']
+
+envVars.forEach(e => {
+  if (!process.env[e]) {
+    throw new Error(`Missing ${e}`)
+  }
+})
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      cookie: {},
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
